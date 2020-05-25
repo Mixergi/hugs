8e3af0d4b25b9c507f47093d01927cfcb460e7dd
Mixergi <41150598+Mixergi@users.noreply.github.com>
2020-05-25 09:45:20 +0900

    #9 create youtuve download code

--- utils/audio.py
+++ utils/audio.py
@@ -1,10 +1,19 @@
 import os
 import youtube_dl
+import librosa
 
 
-
-def youtube_mp3_download(url, download_path):
-    
+def youtube_mp3_download(url, file_name, download_path):
     ydl_opts = {
-
+        'format': 'bestaudio/best',
+        'outtmpl': f'{download_path}/{file_name}.wav',
+        'noplaylist': True,
+        'continue_dl': True
     }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+
+if __name__ == "__main__":
+    youtube_mp3_download('https://www.youtube.com/watch?v=m7pr7LdDX1E', 'test1', '.')
