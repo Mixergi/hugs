98fb5dec6f83cf0cfabffc6e7b0419d394069ca5
DawonChoi <41173953+DawonChoi@users.noreply.github.com>
2020-07-09 17:57:20 +0900

    #37 Create shutdown handler

---
+++ shutdown.py
@@ -0,0 +1,8 @@
+from flask import request
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+shutdown_server()
