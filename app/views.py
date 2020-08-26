11f002fdece691c62d31136589689d3c97365ca6
DawonChoi <41173953+DawonChoi@users.noreply.github.com>
2020-08-26 17:45:53 +0900

    test4

--- app/views.py
+++ app/views.py
@@ -228,7 +228,7 @@ def login():
                     print("[0][0] : ", result[0][0])
                     session["USERNAME"] = str(result[0][0])
                     db.close()
-                    return render_template("public/profile.html", user=user_session_container[username], status='Logout')
+                    return render_template("public/profile.html", user=user_session_container[session["USERNAME"]], status='Logout')
                 else:
                     flash("잘못된 비밀번호입니다.", "warning")
                     return redirect(request.url)
